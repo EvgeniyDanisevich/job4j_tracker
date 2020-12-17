9fefe373a23851a6d4d1bb03ce0d9070daeef705
EvgeniyDanisevich <a937va@gmail.com>
2020-12-17 21:41:11 +0300

    1.7. Состояние объекта [#423820]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryOne = new Battery(10);
+        Battery batteryTwo = new Battery(15);
+        System.out.println("Before exchange: batteryOne " + batteryOne.load + ". " + "batteryTwo " + batteryTwo.load);
+        batteryOne.exchange(batteryTwo);
+        System.out.println("After exchange: batteryOne " + batteryOne.load + ". " + "batteryTwo " + batteryTwo.load);
+    }
+}
