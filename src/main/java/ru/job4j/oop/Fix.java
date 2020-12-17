a830049bd757b383182f3562f393de147463b574
EvgeniyDanisevich <a937va@gmail.com>
2020-12-17 20:29:31 +0300

    1.1.1. Конструктор по умолчанию [#423821]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String string) {
+        desc = string;
+    }
+
+    public static void main(String[] args) {
+        Fix fix = new Fix("Строка");
+    }
+}
