369e023b33de6a7705db000c2f8ec93859891db5
EvgeniyDanisevich <a937va@gmail.com>
2020-12-20 01:23:24 +0300

    9. toString [#423842]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,8 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        tracker.add(new Item(1, "Заявление"));
-        System.out.println(tracker.findById(1).getName());
+        Item item = new Item(1, "Заявление");
+        System.out.println(item.toString());
     }
 }
