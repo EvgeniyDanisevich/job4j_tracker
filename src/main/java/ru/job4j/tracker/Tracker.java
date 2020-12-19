da65291c969610bedaaa6b660316effe60030c54
EvgeniyDanisevich <a937va@gmail.com>
2020-12-19 22:34:04 +0300

    5. Tracker - хранилище [#423843]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,22 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemByName = new Item[items.length];
+        int value = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName().equals(key)) {
+                itemByName[value] = items[i];
+                value++;
+            }
+        }
+        return Arrays.copyOf(itemByName, value);
     }
 
     public Item findById(int id) {
