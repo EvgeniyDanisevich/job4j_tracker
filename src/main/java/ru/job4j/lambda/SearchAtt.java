8a44122dc770c82521db7ce1108d36e3bd1fbdc1
EvgeniyDanisevich <a937va@gmail.com>
2021-01-02 23:41:26 +0300

    2.0. Встроенные функциональные интерфейсы. [#423568]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,38 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchAtt {
+
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        Predicate<Attachment> attachmentPredicate = new Predicate<>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getSize() > 100;
+            }
+        };
+        return filter(list, attachmentPredicate);
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        Predicate<Attachment> attachmentPredicate = new Predicate<>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getName().contains("bug");
+            }
+        };
+        return filter(list, attachmentPredicate);
+    }
+
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> attachmentPredicate) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (attachmentPredicate.test(att)) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+}
