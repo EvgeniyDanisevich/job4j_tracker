180d3092dfaed8ed2db83158a09c9361e2906adc
EvgeniyDanisevich <a937va@gmail.com>
2021-01-05 16:15:29 +0300

    0.1. Отладка лямбда в IDEA [#423581]

--- src/main/java/ru/job4j/lambda/StreamUsage.java
+++ src/main/java/ru/job4j/lambda/StreamUsage.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsage {
-    /*   public static class Task {
+        public static class Task {
         private final String name;
         private final long spent;
 
@@ -23,7 +23,7 @@ public class StreamUsage {
                     + ", spent=" + spent
                     + '}';
         }
-    }*/
+    }
 
     public static void main(String[] args) {
 /*        List<Task> tasks = List.of(
@@ -44,10 +44,23 @@ public class StreamUsage {
 
  */
 
-        List<Integer> integerList = new ArrayList<>();
+/*        List<Integer> integerList = new ArrayList<>();
         Collections.addAll(integerList, -3, 3, -2, 2, -1, 1);
-        List<Integer> onlyPositive = integerList.stream().filter(integer -> integer > 0).
-                collect(Collectors.toList());
+        List<Integer> onlyPositive = integerList.stream()
+                .filter(integer -> integer > 0)
+                .collect(Collectors.toList());
         onlyPositive.forEach(System.out::println);
+        */
+
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
